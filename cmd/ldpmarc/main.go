8e90569067d7ca6cb57ddeb1d07f72b666001065
Nassib Nassar <nrn@etymon.com>
2021-05-22 04:17:51 +0000

    use uuid data type

--- cmd/ldpmarc/main.go
+++ cmd/ldpmarc/main.go
@@ -197,11 +197,11 @@ func setupTable(txout *sql.Tx) error {
 	}
 	q = "" +
 		"CREATE TABLE " + tableout + " (" +
-		"    srs_id varchar(36) NOT NULL," +
+		"    srs_id uuid NOT NULL," +
 		"    line smallint NOT NULL," +
-		"    matched_id varchar(36) NOT NULL," +
-		"    instance_hrid varchar(36) NOT NULL," +
-		"    instance_id varchar(36) NOT NULL," +
+		"    matched_id uuid NOT NULL," +
+		"    instance_hrid varchar(32) NOT NULL," +
+		"    instance_id uuid NOT NULL," +
 		"    field varchar(3) NOT NULL," +
 		"    ind1 varchar(1) NOT NULL," +
 		"    ind2 varchar(1) NOT NULL," +
@@ -214,7 +214,7 @@ func setupTable(txout *sql.Tx) error {
 	}
 	q = "" +
 		"CREATE TABLE IF NOT EXISTS " + tableoutSchema + "." + tablefinalTable + " (" +
-		"    srs_id varchar(36) NOT NULL," +
+		"    srs_id uuid NOT NULL," +
 		"    line smallint NOT NULL," +
 		"    PRIMARY KEY (srs_id, line)" +
 		");"
